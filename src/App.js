41415f02586c446f65d0f2cadd69abc3698306c1
thilankawe <108317482+thilankawe@users.noreply.github.com>
2023-01-24 16:06:56 +0530

    Update App.js
    Add build

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-now i'm  edited
+now i'm  edited with build
         </p>
       </div>
     );
