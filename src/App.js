f2138e23ea083cf2a0e66343e8119cd6f5d8e98c
thilankawe <108317482+thilankawe@users.noreply.github.com>
2023-01-25 13:01:49 +0530

    Update App.js
    Add

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
 now i'm  edited with build again
         </p>
-        <h1>Here</h1>
+        <h1>edited</h1>
       </div>
     );
   }
